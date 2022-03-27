2fdec44e202aaa3288a37e34f48e54a3de152837
Palak Singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-03-27 23:58:31 +0530

    27-03-2022 day 3

---
+++ src/easy/MergeTwoSortedLists.java
@@ -0,0 +1,39 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 24-03-2022
+ */
+
+//Problem 21
+package easy;
+
+import java.util.LinkedList;
+import java.util.List;
+
+class ListNode {
+      int val;
+      ListNode next;
+
+      ListNode() {
+      }
+
+      ListNode(int val) {
+          this.val = val;
+      }
+
+      ListNode(int val, ListNode next) {
+          this.val = val;
+          this.next = next;
+      }
+}
+
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        List<Integer> list1 = new LinkedList<>();
+        list1 = [1, 2, 4];
+
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+    }
+}
