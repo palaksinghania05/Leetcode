bf1e6cc829888259442add97cbbba3386dd1ff19
Palak Singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-07-13 23:57:48 +0530

    added a problem

---
+++ src/easy/AddTwoIntegers.java
@@ -0,0 +1,19 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 13-07-2022
+ */
+
+package easy;
+
+public class AddTwoIntegers {
+    public static void main(String[] args) {
+        int a = -10;
+        int b = 6;
+        System.out.println(sum(a, b));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
