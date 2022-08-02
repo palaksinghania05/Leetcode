e2ac9cf2139465ae7a4a52719d092f80299284ec
Palak Singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-08-02 23:57:20 +0530

    added some problems

---
+++ src/medium/KthSmallestElement.java
@@ -0,0 +1,26 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 02-08-2022
+ */
+
+package medium;
+
+public class KthSmallestElement {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 5, 9}, {10, 11, 13}, {12, 13, 15}};
+        int k = 8;
+        // System.out.println(kthSmallest(matrix, k));
+    }
+
+//    public static int kthSmallest(intpublic int kthSmallest(int[][] matrix, int k) {
+//
+//    }[][] matrix, int k) {
+//        HashSet<Integer> hashset = new HashSet<>();
+//        for (int i = 0; i < matrixn.legth; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//
+//            }
+//        }
+//    }
+}
