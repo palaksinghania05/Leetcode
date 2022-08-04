29b24e5969081394cd8ece9088c56c78370a35e7
Palak Singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-08-04 23:59:04 +0530

    added some problems

---
+++ src/easy/RichestCustomerWealth.java
@@ -0,0 +1,13 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 04-08-2022
+ */
+
+package easy;
+
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+
+    }
+}
