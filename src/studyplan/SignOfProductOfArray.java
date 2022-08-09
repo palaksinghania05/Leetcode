848541f4c3d0491eedf5d6a328ea1fd55207c44b
Palak Singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-08-10 00:06:02 +0530

    added some problems

--- src/studyplan/SignOfProductOfArray.java
+++ src/studyplan/SignOfProductOfArray.java
@@ -7,26 +7,25 @@
 //Product 1822
 package studyplan;
 
+import java.math.BigInteger;
+
 public class SignOfProductOfArray {
     public static void main(String[] args) {
-        int[] arr = {41, 65, 14, 80, 20, 10, 55, 58, 24, 56, 28, 86, 96, 10, 3, 84, 4, 41, 13, 32, 42, 43, 83, 78, 82, 70, 15, -41};
+        int[] arr = {9, 72, 34, 29, -49, -22, -77, -17, -66, -75, -44, -30, -24};
         System.out.println(arraySign(arr));
     }
 
     public static int arraySign(int[] nums) {
-        long product = 1;
+        BigInteger product = BigInteger.ONE;
         for (int i = 0; i < nums.length; i++) {
-            product = product * nums[i];
+            product = product.multiply(BigInteger.valueOf(nums[i]));
+            System.out.println(product);
         }
         return signFunc(product);
     }
 
-    public static int signFunc(long product) {
-        if (product == 0)
-            return 0;
-        else if (product < 0)
-            return -1;
-        else
-            return 1;
+    public static int signFunc(BigInteger product) {
+        int val = product.compareTo(BigInteger.ZERO);
+        return val;
     }
 }
