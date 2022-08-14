fc121ee2a72d3557d70c6aaff0ac7c6fb6a5ee30
palak singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-08-14 22:25:41 +0530

    added some problems

--- src/easy/SameTree.java
+++ src/easy/SameTree.java
@@ -34,10 +34,10 @@ public class SameTree {
     }
 
     public static boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null || q == null)
-            return false;
         if (p == null && q == null)
             return true;
+        if (p == null || q == null)
+            return false;
         if (p.val == q.val)
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         return false;
