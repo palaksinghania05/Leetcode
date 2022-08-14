68f9f8e5bbf7cf906675d8f42418ca6b85e41cb7
palak singhania <palak.singhania_cs.ccv19@gla.ac.in>
2022-08-14 22:25:07 +0530

    added some problems

---
+++ src/easy/SameTree.java
@@ -0,0 +1,45 @@
+/*
+ * Author Name: Palak Singhania
+ * IDE: IntelliJ IDEA Ultimate Edition
+ * Date: 14-08-2022
+ */
+
+//Problem 100
+package easy;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SameTree {
+    public static void main(String[] args) {
+        int[] p = {1, 2, 3};
+        int[] q = {1, 2, 3};
+        System.out.println(isSameTree(new TreeNode(p[0]), new TreeNode(q[0])));
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null)
+            return false;
+        if (p == null && q == null)
+            return true;
+        if (p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return false;
+    }
+}
